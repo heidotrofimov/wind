419e983d2a8d8edb94f46acf92143dd8a5f2b6d6
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-15 14:07:22 +0300

    Update windrose.py

--- windrose.py
+++ windrose.py
@@ -3,11 +3,11 @@ import plotly.graph_objects as go
 fig = go.Figure()
 
 fig.add_trace(go.Barpolar(
-    r=[77.5, 72.5, 70.0, 45.0, 22.5, 42.5, 40.0, 62.5],
+    r=[77.5, 72.5, 70.0, 45.0, 22.5, 42.5, 40.0, 62.5,100,50],
     name='11-14 m/s',
     marker_color='rgb(106,81,163)'
 ))
-fig.update_traces(text=['North', 'N-E', 'East', 'S-E', 'South', 'S-W', 'West', 'N-W'])
+fig.update_traces(text=['North', 'N-E', 'East', 'S-E', 'South', 'S-W', 'West', 'N-W','A','C'])
 fig.update_layout(
     title='Wind Speed Distribution in Laurel, NE',
     font_size=16,
