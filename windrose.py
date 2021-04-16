2684f46defc08b8aace776112eb9a45445866cd8
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-16 11:14:30 +0300

    Update windrose.py

--- windrose.py
+++ windrose.py
@@ -71,7 +71,7 @@ fig.update_layout(
     legend_font_size=16,
     polar_radialaxis_ticksuffix='%',
     polar_angularaxis_rotation=90,
-    direction='counterclockwise',
+    direction='clockwise',
     
    
 
