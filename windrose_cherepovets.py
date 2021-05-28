21edb9665572fb992c4bc26e57c4beb5e3f3e15d
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-05-28 13:42:07 +0300

    Update windrose_cherepovets.py

--- windrose_cherepovets.py
+++ windrose_cherepovets.py
@@ -13,70 +13,42 @@ directions=mock_dir
 #r_cloud=[30.612244897959183,18.367346938775512,4.081632653061225,12.244897959183673,8.16326530612245,4.081632653061225,2.0408163265306123,0.0,6.122448979591836,6.122448979591836,6.122448979591836,2.0408163265306123,2.0408163265306123,16.3265306122449,38.775510204081634,100.0,73.46938775510205,75.51020408163265]
 #r_track=[21.951219512195124,17.073170731707318,7.317073170731707,11.38211382113821,4.0650406504065035,8.94308943089431,6.504065040650407,9.75609756097561,15.447154471544716,22.76422764227642,28.455284552845526,40.65040650406504,47.96747967479675,82.92682926829268,90.2439024390244,100.0,71.54471544715447,47.15447154471545]
 
-r_track=[39.130434782608695, 60.86956521739131, 45.65217391304348, 15.217391304347828, 13.043478260869565, 2.1739130434782608, 6.521739130434782, 0.0, 4.3478260869565215, 8.695652173913043, 15.217391304347828, 34.78260869565217, 32.608695652173914, 47.82608695652174, 86.95652173913044, 100.0, 41.30434782608695]
+tracks= [18, 28, 21,  7,  6,  1,  3,  0,  2,  4,  7, 16, 15, 22, 40, 46, 19]
+clouds= [16,  29,  15,   8,   6,   5,   8,  15,  29,  65,  67, 110, 107,  86, 55,  58,  20]
+zeros=[69, 121,  72,  51, 46,  61,  92, 119, 238, 366, 560, 600, 600, 449, 342, 296, 113]
 
 
 
-r_cloud=[14.545454545454545, 26.36363636363636, 13.636363636363635, 7.2727272727272725, 5.454545454545454, 4.545454545454546, 7.2727272727272725, 13.636363636363635, 26.36363636363636, 59.09090909090909, 60.909090909090914, 100.0, 97.27272727272728, 78.18181818181819, 50.0, 52.72727272727272, 18.181818181818183]
+abs_nr=[]
+cloud_pr=[]
+track_pr=[]
 
+for i in range(len(tracks)):
+    abs_nr.append(tracks[i]+clouds[i]+zeros[i])
+    cloud_pr.append((tracks[i]+clouds[i])/(tracks[i]+clouds[i]+zeros[i]))
+    track_pr.append((tracks[i])/(tracks[i]+clouds[i]))
 
-r_common=[]
-r_track_over=[]
-r_cloud_over=[]
-
-for i in range(len(r_cloud)):
-    cloud=r_cloud[i]
-    track=r_track[i]
-    if(cloud>track):
-        common=track
-        cloud_over=cloud
-        track_over=0
-        new_cloud_over=cloud_over-common
-        r_common.append(common)
-        r_cloud_over.append(new_cloud_over)
-        r_track_over.append(track_over)
-    elif(track>cloud):
-        common=cloud
-        cloud_over=0
-        track_over=track
-        new_track_over=track_over-common
-        r_common.append(common)
-        r_cloud_over.append(cloud_over)
-        r_track_over.append(new_track_over)
-    else:
-        common=track
-        track_over=0
-        cloud_over=0
-        r_common.append(common)
-        r_cloud_over.append(cloud_over)
-        r_track_over.append(track_over)
+if(sys.argv[1]=='all'):
+    choice=abs_nr
+    color='rgba(0,128,0,1.0)'
     
+if(sys.argv[1]=='cloud'):
+    choice=cloud_pr
+    color='rgba(0,0,255,1.0)'
+    
+if(sys.argv[1]=='track'):
+    choice=track_pr
+    color='rgba(255,0,0,1.0)'
+
 
 fig.add_trace(go.Barpolar(
    
     theta = directions,
-    r=r_common,
- 
-    name='Track and cloud days overlapping',
-    marker_color='rgba(255,0,255,1.0)'
+    r=choice,
+    marker_color=color
 ))
 
-fig.add_trace(go.Barpolar(
-    
-    theta = directions,
-    r=r_cloud_over,
-    name='Only cloud days',
-    marker_color='rgba(0,0,255,1.0)'
-))
 
-fig.add_trace(go.Barpolar(
-    
-    theta = directions,
-    r=r_track_over,
-
-    name='Track days',
-    marker_color='rgba(255,0,0,1.0)'
-))
 
 fig.update_layout(
     font_size=36,
@@ -89,7 +61,6 @@ fig.update_layout(
         tickvals = [0, 45, 90, 135, 180, 225,270,315],
         ticktext = ['N', 'NW', 'W', 'SW', 'S', 'SE','E','NE']
     )
-    
    
 
 )
